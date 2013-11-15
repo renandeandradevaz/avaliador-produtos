f0b19ee0880e1c00e629efc47485047f0938db82
renandeandradevaz <renanandrade_rj@hotmail.com>
2013-11-15 16:34:36 -0200

    Creating the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AvaliadorProdutos::Application.config.secret_key_base = '9666665763d43ca9ab0b1dce1bc5d7ba77b8d874f09ce50f365fca2e156db47d759fbe36b2d24f8ae6d03a686612e83c0f8bae2aed6918e9f7d970d9621e5945'
